495040033a27c2fb56725fe0b1acd189791e96fd
rahul rajpurohit <55624973+rahulraj-code@users.noreply.github.com>
2021-06-05 01:36:50 +0530

    Update background.js
    lang added

--- Eli/background.js
+++ Eli/background.js
@@ -14,11 +14,16 @@ var dict  ={
 chrome.contextMenus.create(speak);
 chrome.contextMenus.create(dict);
 
-var change_rate = document.getElementById("rate_slider");
+
 
 chrome.storage.sync.get('speak_rate',function(eli){
-
-    change_rate = eli.speak_rate;
+    if(eli.speak_rate){
+        chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+            if(request.todo == "change_speak_rate" ){
+                change_rate = eli.speak_rate;
+            }
+        });
+    }
 });
 
 var url_dict= "https://www.google.com/search?q=dictionary&rlz=1C1CHBD_enIN853IN853&sxsrf=ALeKk00iVW1zRCkxUWWk2YxsB61O7JUhzA%3A1618673959012&ei=JwF7YNEbxa_1A-efjrAD&oq=dictio&gs_lcp=Cgdnd3Mtd2l6EAMYADIHCAAQsQMQQzIECAAQQzIHCAAQhwIQFDICCAAyBQgAELEDMgIIADIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQM6BwgAEEcQsAM6BAgjECc6DQgAEIcCELEDEIMBEBQ6CAgAELEDEIMBOgcIIxDqAhAnOgoIABCxAxCDARBDOgsILhCxAxDHARCjAjoKCC4QxwEQowIQQzoKCAAQhwIQsQMQFDoFCC4QsQNQtzlYz09gxlVoAnACeACAAZwBiAHgB5IBAzAuOJgBAKABAaoBB2d3cy13aXqwAQrIAQjAAQE&sclient=gws-wiz#dobs="
@@ -26,6 +31,8 @@ var url_dict= "https://www.google.com/search?q=dictionary&rlz=1C1CHBD_enIN853IN8
 function dict_url(str){
     return url_dict + str;
 };
+
+
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if(clickData.menuItemId == dict.id && clickData.selectionText){
@@ -47,15 +54,69 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
 
     };
 
+
     if(clickData.menuItemId == speak.id && clickData.selectionText){
         
-        chrome.runtime.OnMessage.addListener(function(request,sender,sendResponse){
-            
-        });
+        //chrome.tts.speak(clickData.selectionText,{"rate":change_rate});
 
-        chrome.tts.speak(clickData.selectionText,{"rate":change_rate});
+       
+        var curr_rate =parseFloat(localStorage.getItem('speed')); 
+        if(curr_rate!==1)
+        {
+            curr_rate =curr_rate/5;
+        }
+        const curr_lang = localStorage.getItem('lang');
+          console.log("speaking now at rate ",curr_rate," and lang :",curr_lang);
+      
+      
+        
+        
+        chrome.tts.speak(
+            clickData.selectionText,{'rate': curr_rate,'lang':curr_lang},
+            function() {
+              if (chrome.runtime.lastError) {
+                console.log('Error: ' + chrome.runtime.lastError.message);
+              }
+            }
+          );
+
+          
+    
     };
 
 
 });
 
+
+
+
+
+/* 
+name: Google Deutsch
+  1 lang: de-DE
+
+ name: Google US English
+  2lang: en-US
+
+name: Google UK English Female
+  3lang: en-GB
+
+  name: Google español
+   4 lang: es-ES
+
+name: Google français
+   5 lang: fr-FR
+
+ name: Google हिन्दी
+  6 lang: hi-IN
+
+   name: Google italiano
+   7 lang: it-IT
+
+   name: Google 日本語
+   8 lang: ja-JP
+    
+   9name: Google 한국의
+  lang: ko-KR
+
+*/
